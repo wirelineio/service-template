a783ddf16da9cf29d76cbea5b2e681df56523718
Max Fierro <elmasse@gmail.com>
2018-12-11 11:06:54 -0300

    Update deps. Added tests.

--- handler.js
+++ handler.js
@@ -4,11 +4,10 @@ import Wireline from '@wirelineio/sdk';
 module.exports = {
 
   test: Wireline.exec(async (event, context) => {
-    let { name='Wireline' } = event.queryStringParameters || {};
+    let { name = 'Wireline' } = event.queryStringParameters || {};
 
     return {
       message: `Hello, ${name}!`
     };
   })
-
 };
---
+++ handler.test.js
@@ -0,0 +1,26 @@
+const request = require("supertest");
+const { createDevServer } = require("@wirelineio/cli/dev_server");
+
+let server;
+
+beforeAll(async () => {
+  server = await createDevServer();
+});
+
+test("GET /test", done =>
+  request(server)
+    .get("/test")
+    .expect(200)
+    .expect(response =>
+      expect(response.body).toMatchObject({ message: "Hello Wireline" })
+    )
+    .end(err => done(err)));
+
+test("GET /test?name=Bob", done =>
+  request(server)
+    .get("/test?name=Bob")
+    .expect(200)
+    .expect(response =>
+      expect(response.body).toMatchObject({ message: "Hello Bob" })
+    )
+    .end(err => done(err)));
